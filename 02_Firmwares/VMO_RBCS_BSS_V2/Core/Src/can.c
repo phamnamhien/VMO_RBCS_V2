773ae17c2ed823face63200d6a5116a6754965d3
phamnamhien <48169003+phamnamhien@users.noreply.github.com>
2026-02-10 17:01:01 +0700

    Enable CAN IRQs and update CAN timing/settings
    Add CAN interrupt handlers and NVIC setup, and adjust CAN bus parameters. Changes include:
    - Added prototypes for USB_HP_CAN1_TX, CAN1_RX1 and CAN1_SCE in stm32f1xx_it.h and implemented their ISRs in stm32f1xx_it.c (they call HAL_CAN_IRQHandler).
    - Enabled and set priority for USB_HP_CAN1_TX, CAN1_RX1 and CAN1_SCE IRQs (enable/disable in HAL_CAN_MspInit/HAL_CAN_MspDeInit) in can.c.
    - Modified CAN initialization timing: SJW=2TQ, BS1=13TQ, BS2=4TQ (Prescaler remains 2 -> 1Mbps) and toggled features AutoBusOff and AutoRetransmission.
    - Updated CubeMX .ioc to reflect ABOM, SJW, NART and the added NVIC entries.
    These changes enable additional CAN interrupt sources and adjust CAN bus timing/behavior for the target hardware.

--- 02_Firmwares/VMO_RBCS_BSS_V2/Core/Src/can.c
+++ 02_Firmwares/VMO_RBCS_BSS_V2/Core/Src/can.c
@@ -40,13 +40,13 @@ void MX_CAN_Init(void)
   hcan.Instance = CAN1;
   hcan.Init.Prescaler = 2;
   hcan.Init.Mode = CAN_MODE_NORMAL;
-  hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan.Init.TimeSeg1 = CAN_BS1_15TQ;
-  hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
+  hcan.Init.SyncJumpWidth = CAN_SJW_2TQ;
+  hcan.Init.TimeSeg1 = CAN_BS1_13TQ;
+  hcan.Init.TimeSeg2 = CAN_BS2_4TQ;
   hcan.Init.TimeTriggeredMode = DISABLE;
-  hcan.Init.AutoBusOff = DISABLE;
+  hcan.Init.AutoBusOff = ENABLE;
   hcan.Init.AutoWakeUp = DISABLE;
-  hcan.Init.AutoRetransmission = DISABLE;
+  hcan.Init.AutoRetransmission = ENABLE;
   hcan.Init.ReceiveFifoLocked = DISABLE;
   hcan.Init.TransmitFifoPriority = DISABLE;
   if (HAL_CAN_Init(&hcan) != HAL_OK)
@@ -87,8 +87,14 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef* canHandle)
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /* CAN1 interrupt Init */
+    HAL_NVIC_SetPriority(USB_HP_CAN1_TX_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(USB_HP_CAN1_TX_IRQn);
     HAL_NVIC_SetPriority(USB_LP_CAN1_RX0_IRQn, 5, 0);
     HAL_NVIC_EnableIRQ(USB_LP_CAN1_RX0_IRQn);
+    HAL_NVIC_SetPriority(CAN1_RX1_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(CAN1_RX1_IRQn);
+    HAL_NVIC_SetPriority(CAN1_SCE_IRQn, 5, 0);
+    HAL_NVIC_EnableIRQ(CAN1_SCE_IRQn);
   /* USER CODE BEGIN CAN1_MspInit 1 */
 
   /* USER CODE END CAN1_MspInit 1 */
@@ -113,7 +119,10 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
     HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11|GPIO_PIN_12);
 
     /* CAN1 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(USB_HP_CAN1_TX_IRQn);
     HAL_NVIC_DisableIRQ(USB_LP_CAN1_RX0_IRQn);
+    HAL_NVIC_DisableIRQ(CAN1_RX1_IRQn);
+    HAL_NVIC_DisableIRQ(CAN1_SCE_IRQn);
   /* USER CODE BEGIN CAN1_MspDeInit 1 */
 
   /* USER CODE END CAN1_MspDeInit 1 */
